9050ba44c0e18d096ba60ca60ba301b1e63e0610
Admin <admin@Admins-MacBook-Pro.local>
2024-11-24 17:33:10 +0000

    Initial checkin

---
+++ src/main/java/com/gimpact/settlementstub/SettlementstubApplication.java
@@ -0,0 +1,13 @@
+package com.gimpact.settlementstub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementstubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlementstubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gimpact/settlementstub/SettlementstubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimpact.settlementstub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementstubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
